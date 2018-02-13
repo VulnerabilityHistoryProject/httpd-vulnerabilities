8f879c8e79affaf5c6114536871160a73ba18e6c
tnrglantz <tanner.glantz@gmail.com>
2018-02-12 23:32:11 -0500

    created yaml data files

--- scripts/data_extract.py
+++ scripts/data_extract.py
@@ -1,26 +1,56 @@
 """
+
 Script to extract vulnerability data and create skeleton YAML File.
 
+
 """
-import yaml
+
 import csv
 import sys
+import yaml
 
-"""
- readingFile reads in CSV file and data to dictionary
-"""
 
-def readingFile(filename):
+skeletonYaml = "cve-skeleton.yml"
+
+def read_yaml(filepath):
+    with open(filepath, "r") as inputFile:
+        data = yaml.load(inputFile)
+    return data
+
+def write_yaml(filepath, data):
+    with open(filepath, "w") as outputFile:
+        yaml.dump(data, outputFile)
+
+
+"""  read_CSV_File reads in CSV file and data to dictionary """
+
+def read_CSV_File(filename):
     inputFile = open(str(filename), 'rb')
     reader = csv.reader(inputFile)
     for row in reader:
         if "CVE-" in row[0]:
             print(row[0])
+            cve_entry = row[0]
+            outputfile = open((str(cve_entry) + ".yml"),"w" )
+            secondFile = open(skeletonYaml, "r")
+            fileLine = secondFile.readline()
+            while fileLine:
+                if "CVE:" in fileLine:
+                    outputfile.write("CVE: " + cve_entry + "\n")
+                else:
+                    outputfile.write(fileLine)
+                fileLine = secondFile.readline()
+
+            outputfile.close()
 """
 Main method
 """
 
 if __name__ == '__main__':
     print("###\nScript to extract vulnerability data and create skeleton YAML file.\n###")
-    # reading in file "httpdData.csv"
-    readingFile("../vul_data/httpd-vulnerabilities.csv") 
+    vul_data_file = "../vul_data/httpd-vulnerabilities.csv"
+#    data = read_yaml(skeletonYaml)
+#    print(data)
+    read_CSV_File(vul_data_file)
+    
+ 
