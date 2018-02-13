647565216e7681edb3de5c6ee23b678a8d820643
tnrglantz <tanner.glantz@gmail.com>
2018-02-12 20:53:54 -0500

    pulled out cve lists from file

--- scripts/data_extract.py
+++ scripts/data_extract.py
@@ -3,13 +3,24 @@ Script to extract vulnerability data and create skeleton YAML File.
 
 """
 import yaml
+import csv
+import sys
 
-#def createYaml():
+"""
+ readingFile reads in CSV file and data to dictionary
+"""
 
-yaml.load("""
-- Hesper
-- Papa
-- Apaa
-- Epe
- """)
+def readingFile(filename):
+    inputFile = open(str(filename), 'rb')
+    reader = csv.reader(inputFile)
+    for row in reader:
+        if "CVE-" in row[0]:
+            print(row[0])
+"""
+Main method
+"""
 
+if __name__ == '__main__':
+    print("###\nScript to extract vulnerability data and create skeleton YAML file.\n###")
+    # reading in file "httpdData.csv"
+    readingFile("../vul_data/httpd-vulnerabilities.csv") 
