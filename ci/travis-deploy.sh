eb7f5d385c333fa79b198d267b7b8e7729834ddb
tnrglantz <8582756+tanner-g@users.noreply.github.com>
2018-09-18 21:32:13 -0400

    saving work... rakefile under construction

--- ci/travis-deploy.sh
+++ ci/travis-deploy.sh
@@ -5,5 +5,5 @@ echo "Travis Testing Script"
 # Script should exit when something fails, but also print out all commands ran
 set -ev
 
-#running unit tests
-python unitTests.py
+#runs cve unit test check
+ruby cve_unit_test.rb
