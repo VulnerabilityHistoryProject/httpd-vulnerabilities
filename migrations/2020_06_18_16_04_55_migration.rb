ac71ed12f0c3b5de0c5530b92f04a9d8796b3e9a
Andy Meneely <andy@se.rit.edu>
2020-06-18 21:42:50 -0400

    next step working

--- migrations/2020_06_18_16_04_55_migration.rb
+++ migrations/2020_06_18_16_04_55_migration.rb
@@ -10,7 +10,7 @@ require 'open3'
 # Make sure you have the latest VHP shepherd tools installed
 # Run this from the top of the repo.
 
-@@GITLOG_JSON = JSON.parse(File.read('commits/gitlog.json'))
+@gitlog_json = JSON.parse(File.read('commits/gitlog.json'))
 
 def order_of_keys
   %w(
@@ -55,28 +55,32 @@ def order_of_keys
 end
 
 def commit_exists?(sha)
-  stdout, stderr, status = Open3.capture3("git -C ./tmp/src show #{sha} --")
+  stdout, stderr, status = Open3.capture3("git -C ./tmp/src show #{sha} -- ")
   return true if stderr.strip.empty?
-  return false if stderr.matches? /fatal: bad revision/
+  return false if stderr.match? /fatal: bad object/
   # Otherwise... uh oh
   warn "ERROR looking up #{sha} failed. See message below. Skipping commit"
-  warn "ERROR Original git message: #{e.message}"
+  warn "ERROR Original git message: #{stderr}"
   return true
 end
 
 # Sometimes Apache HTTPD updates their
 def update_commitlist(h)
   h.map do |entry|
-    c = entry[:commit].to_s.strip
-    n = entry[:note]
-    entry
+    sha = entry["commit"].to_s.strip
+    n = entry["note"]
     # First, try to look it up in our existing repo
-    # if commit_exists?(commit) || c.empty?
-    #   entry # do nothing, put it back in the hash
-    # else
-    #   # Ok, we know it doesn't exist. Now look it up in gitlog.json
-    #
-    # end
+    if commit_exists?(sha) || sha.empty?
+      entry # do nothing, put it back in the hash
+    else
+      # Ok, we know it doesn't exist. Now look it up in gitlog.json
+      if @gitlog_json.key? sha
+        "WE'VE GOT IT!!"
+      else
+        print 'X'
+        "UH OH"
+      end
+    end
   end
 end
 
@@ -88,6 +92,8 @@ ymls.each do |yml_file|
   h["fixes"] = update_commitlist(h["fixes"])
   # h[:vccs] = update_commitlist(h[:vccs])
   # h[:interesting_commits][:commits] = update_commitlist(h[:interesting_commits][:commits])
+
+
 
   # Reconstruct the hash in the order we specify
   out_h = {}
